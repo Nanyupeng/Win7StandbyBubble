abfef7556ffb1b30efa61b249c99d908dbe34316
Nan Yupeng <38316241+Nanyupeng@users.noreply.github.com>
2020-03-28 11:41:35 +0800

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Awake()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(transform.up * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "wall")
+        {
+            float angleValue = Vector3.Angle(transform.up, other.transform.up);
+            Vector3 reflect = Vector3.Reflect(transform.position, other.transform.position);
+            transform.position = reflect;
+            if (angleValue < 90)
+            {
+                transform.eulerAngles += new Vector3(0, 0, 2 * angleValue);
+            }
+            else if (Vector3.Angle(transform.up, other.transform.up) > 90)
+            {
+                transform.eulerAngles -= new Vector3(0, 0, 360 - 2 * angleValue);
+            }
+            else
+            {
+                transform.eulerAngles += new Vector3(0, 0, 180);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Move2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Vector2 m_preVelocity = new Vector2(4, 0);
+    // Use this for initialization
+    void Start()
+    {
+        rig = transform.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(transform.up * 50f * Time.deltaTime, Space.Self);
+    }
+
+    //private void OnMouseDown()
+    //{
+    //    rig.AddForce(new Vector2(1, 0) * 200);
+    //}
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        ContactPoint2D contactPoint = other.contacts[0];//获取接触点
+        Vector2 newDir = Vector2.zero;
+        Vector2 curDir = transform.TransformDirection(Vector2.right);
+        newDir = Vector2.Reflect(curDir, contactPoint.normal);//计算反射角
+        Quaternion rotation = Quaternion.FromToRotation(Vector2.right, newDir);
+        transform.rotation = rotation;
+        Debug.Log(newDir);
+        //rig.velocity = newDir.normalized * m_preVelocity.x / m_preVelocity.normalized.x;
+    }
+}
